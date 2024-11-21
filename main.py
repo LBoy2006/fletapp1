46732e6abb854912b4491f78ab0d567083767ebb
LBoy2006 <93949151+LBoy2006@users.noreply.github.com>
2024-11-21 23:55:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,14 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.title = "Flet counter example"
-    page.theme_mode = ft.ThemeMode.LIGHT
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
-    txt_number = ft.TextField(value="0", text_align=ft.TextAlign.RIGHT, width=100)
-
-    def minus_click(e):
-        txt_number.value = str(int(txt_number.value) - 1)
-        page.update()
-
-    def plus_click(e):
-        txt_number.value = str(int(txt_number.value) + 1)
-        page.update()
-
+    audio1 = ft.Audio(
+        src="https://luan.xyz/files/audio/ambient_c_motion.mp3", autoplay=True
+    )
+    page.overlay.append(audio1)
     page.add(
-        ft.Row(
-            [
-                ft.IconButton(ft.icons.REMOVE, on_click=minus_click),
-                txt_number,
-                ft.IconButton(ft.icons.ADD, on_click=plus_click),
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-        )
+        ft.Text("This is an app with background audio."),
+        ft.ElevatedButton("Stop playing", on_click=lambda _: audio1.pause()),
     )
 
-
-ft.app(main, view=ft.AppView.WEB_BROWSER)
+ft.app(main, #view=ft.AppView.WEB_BROWSER
+       )
