6621e2579f4709b49c622659d25a2032960882e9
LBoy2006 <lboy2006@ya.ru>
2025-06-20 01:40:30 +0300

    корсы под гитхаб

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: './',
-  server: {
-    allowedHosts: [
-      'lboy2006.github.io/fletapp1'
-    ],
-    // если у тебя уже есть другие опции — просто добавь allowedHosts в server
-  },
+
 });
