67a2fceb1853b6ead7cd0f9c842771ca71592fbf
LBoy2006 <lboy2006@ya.ru>
2025-06-19 23:28:38 +0300

    бд

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # временно для разработки
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
