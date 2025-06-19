64225066ae04471e36fa066b6b100e8c7761d1e7
LBoy2006 <lboy2006@ya.ru>
2025-06-19 23:24:10 +0300

    бд

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://1chn.gptbrainbot.ru",
-    "http://172.31.27.43:5173"
+    "http://172.31.27.43:5173",
+    "https://lboy2006.github.io/fletapp1"
 ]
 
 app.add_middleware(
