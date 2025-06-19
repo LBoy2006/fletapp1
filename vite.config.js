b6b1ec23f03de22d8381e3752c0adf38d1e39937
LBoy2006 <lboy2006@ya.ru>
2025-06-19 22:27:11 +0300

    кнопки

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
   plugins: [vue()],
   base: './',
+  server: {
+    allowedHosts: [
+      '1chn.gptbrainbot.ru'
+    ],
+    // если у тебя уже есть другие опции — просто добавь allowedHosts в server
+  },
 });
