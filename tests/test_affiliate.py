20fd4d1dd606c49dcebf56b347c2f8db19243734
LBoy2006 <93949151+LBoy2006@users.noreply.github.com>
2025-06-30 03:29:08 +0300

    Add affiliate join date and countdown (#172)

--- tests/test_affiliate.py
+++ tests/test_affiliate.py
@@ -3,7 +3,9 @@ def test_read_affiliate(client, db_session):
     uid = client.app.state.user_id
     resp = client.get(f'/affiliate/{uid}')
     assert resp.status_code == 200
-    assert resp.json()['user_id'] == uid
+    data = resp.json()
+    assert data['user_id'] == uid
+    assert data['join_date'] == '2024-01-01'
 
 
 def test_affiliate_not_found(client, db_session):
